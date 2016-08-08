47323fc3b27b0ffd16c221b622aa90765fffae37
Eyakub <eyakubsorkar@gmail.com>
2016-08-08 14:36:25 +0600

    n to m prime.

--- prime from n to n.cpp
+++ prime from n to n.cpp
@@ -6,21 +6,21 @@ int main()
 {
     int f, l, arr[100], i, j, z = 0, p;
     scanf("%d %d", &f, &l);
-    for(i = f; i <= l; i++){
+    for(i = f; i <= l; i++){ ///this loop for first number to last number.
         p = 0;
-        if(i == 1){
+        if(i == 1){ ///this condition cause 1 is not a prime number.
             p == 1;
         }
         else{
             for(j = 2; j < i; j++){
-                if(i % j == 0){
+                if(i % j == 0){ ///the value of i's  will be check here for prime or not
                     p = 1;
                     break;
                 }
             }
         }
         if(p == 0){
-            arr[z++] = i;
+            arr[z++] = i; ///storing prime number's
         }
     }
     for(i = 0; i < z-1; i++){
